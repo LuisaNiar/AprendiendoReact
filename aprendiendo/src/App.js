45d0152b793c9cd995812e4b5d4a8c2088a6a26f
LuisaNiar <luisaniar@unisabana.edu.co>
2023-11-20 17:48:34 -0500

    Inicio

--- aprendiendo/src/App.js
+++ aprendiendo/src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
 import {useState} from "react";
+import Dado from "./Dado";
 
 function App() {
 
     function generarAleatorios() {
-        const vector = new Array(5);
-        for (let x = 0; x < vector.length; x++) {
-            vector[x] = Math.trunc(Math.random() * 10);
-        }
-        setNumeros(vector);
+        const valor1 = Math.trunc(Math.random() * 6) + 1;
+        const valor2 = Math.trunc(Math.random() * 6) + 1;
+        const valor3 = Math.trunc(Math.random() * 6) + 1;
+        return (
+            <div>
+                <Dado valor={valor1}/>
+
+            </div>
+        );
     }
-
-    const [numeros, setNumeros] = useState([0, 0, 0, 0, 0])
-
-    return (
-        <div className="App">
-            <p>Numeros aleatorios</p>
-            {numeros.map(num => (
-                <p> {num}</p>
-            ))}
-            <button onClick={generarAleatorios}>Generar valores aleatorios</button>
-        </div>
-    );
 }
+
+/*
+const vector = new Array(5);
+for (let x = 0; x < vector.length; x++) {
+    vector[x] = Math.trunc(Math.random() * 10);
+}
+setNumeros(vector);
+}
+
+const [numeros, setNumeros] = useState([0, 0, 0, 0, 0])
+
+return (
+<div className="App">
+    <p>Numeros aleatorios</p>
+    {numeros.map(num => (
+        <p> {num}</p>
+    ))}
+    <button onClick={generarAleatorios}>Generar valores aleatorios</button>
+</div>
+
+ */
+
 
 export default App;
---
+++ aprendiendo/src/Dado.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../src/Dado.css'
+
+function Dado(props){
+    return(
+        <div className='dado-recuadro'>
+            {props.valor}
+        </div>
+    )
+}
+
+export default Dado
